78ee31f36750c743ff8deb44ba70d82aeaa068d3
pedrotiagopontes <pedrotiagopontes@gmail.com>
2017-11-05 11:48:45 +0000

    Refactored configuration controller tests to use mocking

--- ToggleAPI.UnitTests/ConfigurationControllerTests.cs
+++ ToggleAPI.UnitTests/ConfigurationControllerTests.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using ToggleAPI.Controllers;
-using ToggleAPI.DAL;
 using ToggleAPI.Interface;
 using ToggleAPI.Mapping;
 using ToggleAPI.Models;
@@ -14,19 +15,12 @@ namespace ToggleAPI.UnitTests
     [TestClass]
     public class ConfigurationControllerTests
     {
-        private IToggleRepository _repository;
+        private Mock<IToggleRepository> _repositoryMock;
 
         [TestInitialize]
         public void Initialize()
         {
-            DbContextOptions<ToggleContext> options = new DbContextOptionsBuilder<ToggleContext>()
-                .UseInMemoryDatabase(databaseName: "Toggle API")
-                .Options;
-
-            var context = new ToggleContext(options);
-            context.EnsureSeedDataForContext();
-
-            _repository = new ToggleRepository(context);
+            _repositoryMock = new Mock<IToggleRepository>();
 
             var cfg = new ToggleMappingConfiguration();
             AutoMapper.Mapper.Initialize(cfg.ConfigurationAction);
@@ -35,133 +29,259 @@ namespace ToggleAPI.UnitTests
         [TestMethod]
         public void GetConfigurationsForToggle_Ok()
         {
-            Assert.Fail();
+            var id = new Guid();
+            var toggle = new Toggle
+            {
+                Id = id,
+                Name = "test",
+                Configurations = new List<Configuration>()
+            };
+            _repositoryMock.Setup(rep => rep.Get(id)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.GetConfigurationsForToggle(id);
+            var actionResult = result as OkObjectResult;
+            var model = actionResult?.Value as IEnumerable<ConfigurationDtoOutput>;
+
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(model);
+            Assert.AreEqual((int)HttpStatusCode.OK, actionResult.StatusCode);
+
         }
 
         [TestMethod]
         public void GetConfigurationsForToggle_ToggleNotFound()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.GetConfigurationsForToggle(new Guid());
             var objectResult = result as NotFoundResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(404, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void GetConfigurationForToggle_Ok()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration> { new Configuration { Id = configurationId } }
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.GetConfigurationForToggle(toggleId, configurationId);
+            var actionResult = result as OkObjectResult;
+            var model = actionResult?.Value as ConfigurationDtoOutput;
+
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(model);
+            Assert.AreEqual((int)HttpStatusCode.OK, actionResult.StatusCode);
         }
 
         [TestMethod]
         public void GetConfigurationForToggle_ToggleNotFound()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.GetConfigurationForToggle(new Guid(), new Guid());
             var objectResult = result as NotFoundResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(404, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void GetConfigurationForToggle_ConfigurationNotFound()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration>()
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.GetConfigurationForToggle(toggleId, configurationId);
+            var objectResult = result as NotFoundResult;
+
+            Assert.IsNotNull(objectResult);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void CreateConfigurationForToggle_Ok()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var toggle = new Toggle();
+            var configurationInput = new ConfigurationDtoInput();
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.CreateConfigurationForToggle(toggleId, configurationInput);
+            var actionResult = result as CreatedAtRouteResult;
+            var model = actionResult?.Value as ConfigurationDtoOutput;
+
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(model);
+            _repositoryMock.Verify(rep => rep.Save(), Times.Once);
+            Assert.AreEqual((int)HttpStatusCode.Created, actionResult.StatusCode);
         }
 
         [TestMethod]
         public void CreateConfigurationForToggle_BadRequest()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.CreateConfigurationForToggle(new Guid(), null);
             var objectResult = result as BadRequestResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(400, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void CreateConfigurationForToggle_ToggleNotFound()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.CreateConfigurationForToggle(new Guid(), new ConfigurationDtoInput());
             var objectResult = result as NotFoundResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(404, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void DeleteConfigurationForToggle_Successful()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var configuration = new Configuration {Id = configurationId};
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration> { configuration }
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.DeleteConfigurationForToggle(toggleId, configurationId);
+            var actionResult = result as NoContentResult;
+
+            Assert.IsNotNull(actionResult);
+            _repositoryMock.Verify(rep => rep.Save(), Times.Once);
+            Assert.AreEqual((int)HttpStatusCode.NoContent, actionResult.StatusCode);
         }
 
         [TestMethod]
         public void DeleteConfigurationForToggle_ToggleNotFound()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.DeleteConfigurationForToggle(new Guid(), new Guid());
             var objectResult = result as NotFoundResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(404, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void DeleteConfigurationForToggle_ConfigurationNotFound()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration>()
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.DeleteConfigurationForToggle(toggleId, configurationId);
+            var objectResult = result as NotFoundResult;
+
+            Assert.IsNotNull(objectResult);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void UpdateConfigurationForToggle_Successful()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var configuration = new Configuration { Id = configurationId };
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration> { configuration }
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result =
+                controller.UpdateConfigurationForToggle(toggleId, configurationId, new ConfigurationDtoInput());
+            var objectResult = result as NoContentResult;
+
+            Assert.IsNotNull(objectResult);
+            Assert.AreEqual((int)HttpStatusCode.NoContent, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void UpdateConfigurationForToggle_BadRequest()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.UpdateConfigurationForToggle(new Guid(), new Guid(), null);
             var objectResult = result as BadRequestResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(400, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void UpdateConfigurationForToggle_ToggleNotFound()
         {
-            var controller = new ConfigurationController(_repository);
+            var controller = new ConfigurationController(_repositoryMock.Object);
 
             var result = controller.UpdateConfigurationForToggle(new Guid(), new Guid(), new ConfigurationDtoInput());
             var objectResult = result as NotFoundResult;
 
             Assert.IsNotNull(objectResult);
-            Assert.AreEqual(404, objectResult.StatusCode);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
 
         [TestMethod]
         public void UpdateConfigurationForToggle_ConfigurationNotFound()
         {
-            Assert.Fail();
+            var toggleId = new Guid();
+            var configurationId = new Guid();
+            var toggle = new Toggle
+            {
+                Id = toggleId,
+                Name = "test",
+                Configurations = new List<Configuration>()
+            };
+            _repositoryMock.Setup(rep => rep.Get(toggleId)).Returns(toggle);
+            var controller = new ConfigurationController(_repositoryMock.Object);
+
+            var result = controller.UpdateConfigurationForToggle(toggleId, configurationId, new ConfigurationDtoInput());
+            var objectResult = result as NotFoundResult;
+
+            Assert.IsNotNull(objectResult);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, objectResult.StatusCode);
         }
     }
 }
